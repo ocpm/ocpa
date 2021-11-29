eabfe695e4ff3cc60ce99d5e45516bb87a792493
niklasadams <niklas.adams@pads.rwth-aachen.de>
2021-11-29 15:32:05 +0100

    tester altering

--- ocpa_tester-private.py
+++ ocpa_tester-private.py
@@ -1,16 +1,19 @@
-from ocpa.objects.ocel.obj import OCEL
+import time
+
+from ocpa.objects.log.obj import OCEL
 from ocpa.algo.discovery.ocpn import algorithm as ocpn_discovery_factory
 import pandas as pd
 import ocpa.algo.filtering.log.trace_filtering as trace_filtering
 import ocpa.algo.evaluation.precision_and_fitness.utils as evaluation_utils
 import ocpa.algo.evaluation.precision_and_fitness.evaluator as precision_fitness_evaluator
 import ocpa.visualization.oc_petri_net.factory as vis_factory
+import ocpa.visualization.log.variants.factory as log_viz
 # TODO: Preprocessing and conversion from other types of OCEL
 filename = "BPI2017.csv"
 ots = ["application", "offer"]
 
 
-event_df = pd.read_csv(filename, sep=',')[:2000]
+event_df = pd.read_csv(filename, sep=',')[:10000]
 print(event_df)
 for ot in ots:
     event_df[ot] = event_df[ot].map(
@@ -19,14 +22,20 @@ event_df["event_id"] = list(range(0, len(event_df)))
 event_df.index = list(range(0, len(event_df)))
 event_df["event_id"] = event_df["event_id"].astype(float).astype(int)
 ocel = OCEL(event_df, ots)
+t_start = time.time()
 print("Number of cases: "+str(len(ocel.cases)))
 print("Number of variants: "+str(len(ocel.variants)))
+print(str(time.time()-t_start))
 print(ocel.variant_frequency)
-sub_ocel = trace_filtering.filter_infrequent_traces(ocel, 0.3)
-ocpn = ocpn_discovery_factory.apply(sub_ocel, parameters={"debug": False})
-contexts, bindings = evaluation_utils.calculate_contexts_and_bindings(ocel)
-precision, fitness = precision_fitness_evaluator.apply(
-    ocel, ocpn, contexts=contexts, bindings=bindings)
-print("Precision: "+str(precision))
-print("Fitness: "+str(fitness))
+t_start = time.time()
+vars = log_viz.apply(ocel)
+print(str(time.time()-t_start))
+print(vars)
+#sub_ocel = trace_filtering.filter_infrequent_traces(ocel, 0.3)
+#ocpn = ocpn_discovery_factory.apply(sub_ocel, parameters={"debug": False})
+#contexts, bindings = evaluation_utils.calculate_contexts_and_bindings(ocel)
+#precision, fitness = precision_fitness_evaluator.apply(
+#    ocel, ocpn, contexts=contexts, bindings=bindings)
+#print("Precision: "+str(precision))
+#print("Fitness: "+str(fitness))
 
