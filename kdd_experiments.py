54527b65fa2a9caea7973b606c657cf3f017496a
niklasadams <niklas.adams@pads.rwth-aachen.de>
2022-01-17 18:53:50 +0100

    added bar plot experiment for isomorphism

--- kdd_experiments.py
+++ kdd_experiments.py
@@ -14,6 +14,7 @@ import ocpa.algo.filtering.log.case_filtering as execution_filtering
 import ocpa.objects.log.importer.ocel.factory as import_factory
 import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.patches as mpatches
 from multiprocessing.dummy import Pool as ThreadPool
 sns.set(style='ticks', palette='Set2')
 sns.despine()
@@ -187,6 +188,52 @@ print("______________")
 print("Isomorphism comparison")
 print("______________")
 print("______________")
+
+
+def results_two_steps_iso(ind, datasets, types):
+    random.seed(a=33)
+    results = {}
+    # Running times of extraction for different subsizes of each log and for different extraction techniques
+    i = ind
+    ds = datasets[i]
+    print("_____________")
+    print(ds)
+    print("_____________")
+    ts = types[i]
+    event_df = None
+    if ds.endswith(".csv"):
+        event_df = pd.read_csv(ds)
+        print(event_df)
+        for t in ts:
+            event_df[t] = event_df[t].map(
+                lambda x: [y.strip() for y in x.split(',')] if isinstance(x, str) else [])
+        event_df["event_id"] = list(range(0, len(event_df)))
+        event_df.index = list(range(0, len(event_df)))
+
+    elif ds.endswith(".jsonocel"):
+        event_df = import_factory.apply(ds, import_factory.OCEL_JSON, parameters={"return_df": True})[0]
+        for t in ts:
+            event_df.loc[event_df[t].isnull(), [t]] = event_df.loc[event_df[t].isnull(), t].apply(lambda x: [])
+    event_df["event_id"] = event_df["event_id"].astype(float).astype(int)
+    execution_extraction_parameters = [("weakly", "")] + [("leading", t) for t in ts]
+    for technique, t in execution_extraction_parameters:
+        s_time = time.time()
+        print("TECHNIQUE: " + technique + " " + t)
+        ocel = None
+        if ind == 1:
+            ocel = OCEL(event_df, ts, execution_extraction=technique, leading_object_type=t,
+                    variant_extraction="complex")
+        else:
+            ocel = OCEL(event_df, ts, execution_extraction=technique, leading_object_type=t,
+                        variant_extraction="complex")
+        print("Number of cases: " + str(len(ocel.cases)))
+        ocel.variant_timeout = 10000
+        s_time = time.time()
+        r_full_time = 0
+        n_before, n_after, t_first, t_second = ocel.calculate_variants_with_data()
+        results[(ds, technique, t)] = (n_before,n_after,t_first,t_second)
+    return results
+
 
 
 def scalability_iso(ind, datasets, types):
@@ -443,36 +490,69 @@ if x:
 
         plt.savefig("iso_sns_"+str(datasets.index(ds_)+1)+".png")
 
+if False:
+    pool = ThreadPool(4)
+    result = pool.starmap(scalability_iso,zip([0,1,2,3],itertools.repeat(datasets),itertools.repeat(types)))
+    print(result)
+    running_times = {k: v for d in result for k, v in d.items()}
+    #Threadsafe plotting to ensure
+    colors = ['#1F77B4', '#FF7F0E', '#2CA02C', '#D62728', '#9467BD', '#8C564B', '#CFECF9', '#7F7F7F', '#BCBD22', '#17BECF']
+    color_map = {"complex": colors[0], "naive": colors[1]}
+    symbols = [None, "o", "v", ".", "^", "<", ">"]
+    sns.set(rc={'figure.figsize': (24, 8)})
+    plt.rcParams["axes.labelsize"] = 16
+    plt.rcParams["axes.titlesize"] = 20
 
-pool = ThreadPool(4)
-result = pool.starmap(scalability_iso,zip([0,1,2,3],itertools.repeat(datasets),itertools.repeat(types)))
-print(result)
-running_times = {k: v for d in result for k, v in d.items()}
-#Threadsafe plotting to ensure
-colors = ['#1F77B4', '#FF7F0E', '#2CA02C', '#D62728', '#9467BD', '#8C564B', '#CFECF9', '#7F7F7F', '#BCBD22', '#17BECF']
-color_map = {"complex": colors[0], "naive": colors[1]}
-symbols = [None, "o", "v", ".", "^", "<", ">"]
-sns.set(rc={'figure.figsize': (24, 8)})
-plt.rcParams["axes.labelsize"] = 16
-plt.rcParams["axes.titlesize"] = 20
+    plt.figure(figsize=(9, 5))
+    for ds_ in datasets:
+        plt.clf()
+        for iso_technique in ["naive","complex"]:
+            x = []
+            y = []
+            #collect data
+            for (ds,technique,t,isomorphism) in running_times.keys():
+                if ds != ds_:
+                    continue
+                if isomorphism != iso_technique:
+                    continue
+                else:
+                    x += [elem[0] for elem in running_times[(ds, technique, t, isomorphism)]]
+                    y += [elem[1] for elem in running_times[(ds,technique,t,isomorphism)]]
 
-plt.figure(figsize=(9, 5))
-for ds_ in datasets:
+            ext_params = [("weakly", "")] + [("leading", t) for t in types[datasets.index(ds)]]
+            pointer_map = {ext_params[i]: symbols[i] for i in range(0, len(ext_params))}
+            #x = [elem[0] for elem in running_times[(ds,technique,t,isomorphism)]]
+            #y = [elem[1] for elem in running_times[(ds,technique,t,isomorphism)]]
+            sns.scatterplot(x, y, color=color_map[iso_technique], marker="o",
+                         label="baseline" if iso_technique == "naive" else "two step approach")#) + ("leading type" +t) if t != "" else "weakly con. comp.")
+            # plt.plot(x,y,color=color_map[ds_], marker=pointer_map[(technique,t)])
+        sns.despine()
+        plt.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
+        plt.xlabel("Number of Process Executions")
+        plt.ylabel("Calculation Equivalence Classes in s")
+        plt.title("Running Time for Computing Equivalence Classes, DS"+str(datasets.index(ds_)+1))
+        plt.tight_layout()
+
+        plt.savefig("iso_sns_final_"+str(datasets.index(ds_)+1)+".png")
+
+
+
+
+#ISOMORPHISM RUNNING TIMES
+
     plt.clf()
     for iso_technique in ["naive","complex"]:
+        #collect data
         x = []
         y = []
-        #collect data
         for (ds,technique,t,isomorphism) in running_times.keys():
-            if ds != ds_:
-                continue
             if isomorphism != iso_technique:
                 continue
             else:
                 x += [elem[0] for elem in running_times[(ds, technique, t, isomorphism)]]
                 y += [elem[1] for elem in running_times[(ds,technique,t,isomorphism)]]
 
-        ext_params = [("weakly", "")] + [("leading", t) for t in types[datasets.index(ds)]]
+
         pointer_map = {ext_params[i]: symbols[i] for i in range(0, len(ext_params))}
         #x = [elem[0] for elem in running_times[(ds,technique,t,isomorphism)]]
         #y = [elem[1] for elem in running_times[(ds,technique,t,isomorphism)]]
@@ -483,42 +563,78 @@ for ds_ in datasets:
     plt.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
     plt.xlabel("Number of Process Executions")
     plt.ylabel("Calculation Equivalence Classes in s")
-    plt.title("Running Time for Computing Equivalence Classes, DS"+str(datasets.index(ds_)+1))
+    plt.title("Running Time for Computing Equivalence Classes")
     plt.tight_layout()
 
-    plt.savefig("iso_sns_final_"+str(datasets.index(ds_)+1)+".png")
+    plt.savefig("iso_sns_final.png")
 
-#full graph of all
+#Dissassmebled runnign times isomorphism
+if True:
+    colors = ['#1F77B4', '#FF7F0E', '#2CA02C', '#D62728', '#9467BD', '#8C564B', '#CFECF9', '#7F7F7F', '#BCBD22',
+              '#17BECF']
+    results_df = []
+    pool = ThreadPool(4)
+    result = pool.starmap(results_two_steps_iso, zip([0, 1, 2, 3], itertools.repeat(datasets), itertools.repeat(types)))
+    print(result)
+    results = {k: v for d in result for k, v in d.items()}
+    for ds in datasets:
+        ds_counter = 0
+        for (ds_,technique,t) in results.keys():
+            if ds_ != ds:
+                continue
+            ds_counter+=1
+            ds_ind = datasets.index(ds)+1
+            results_df.append({'dataset':"DS"+str(ds_ind),
+                               'dataset_x': "DS" + str(ds_ind)+"_"+technique+" "+t,
+                               'hue':str(ds_counter),
+                               'technique':technique+t,
+                               'first_step':results[(ds_,technique,t)][2]/(results[(ds_,technique,t)][2]+results[(ds_,technique,t)][3]),
+                               'second_step':1,#results[(ds_,technique,t)][3]/(results[(ds_,technique,t)][2]+results[(ds_,technique,t)][3]),
+                               'first_by_second':results[(ds_,technique,t)][0]/results[(ds_,technique,t)][1]})
+    results_df = pd.DataFrame(results_df)
+    print(results_df)
+    sns.set(rc={'figure.figsize': (24, 8)})
+    plt.rcParams["axes.labelsize"] = 16
+    plt.rcParams["axes.titlesize"] = 20
 
-plt.clf()
-for iso_technique in ["naive","complex"]:
-    #collect data
-    x = []
-    y = []
-    for (ds,technique,t,isomorphism) in running_times.keys():
-        if isomorphism != iso_technique:
-            continue
-        else:
-            x += [elem[0] for elem in running_times[(ds, technique, t, isomorphism)]]
-            y += [elem[1] for elem in running_times[(ds,technique,t,isomorphism)]]
+    plt.figure(figsize=(10, 5))
 
+    palette = {"1": colors[1], "2": colors[1], "3": colors[1], "4": colors[1], "5": colors[1], "6": colors[1], "7": colors[1]}
+    bar1 = sns.barplot(x="dataset", y="second_step", hue="hue", data=results_df, color=colors[1],  palette=palette)
+    palette = {"1": colors[0], "2": colors[0], "3": colors[0], "4": colors[0], "5": colors[0], "6": colors[0],
+               "7": colors[0]}
+    bar2 = sns.barplot(x="dataset", y="first_step",hue="hue", data=results_df, color=colors[0],  palette=palette)
+    top_bar = mpatches.Patch(color=colors[1], label='Second Step')
+    bottom_bar = mpatches.Patch(color=colors[0], label='First Step')
+    plt.legend(handles=[top_bar, bottom_bar])
+    plt.legend(bbox_to_anchor=(.01, 1), loc=2, borderaxespad=0.)
+    plt.legend(handles=[top_bar, bottom_bar],loc='lower right')
+    plt.xlabel("Data Sets and Different Extraction Techniques")
+    plt.ylabel("Relative Share of Computation Time")
+    plt.title("Distribution of Running Time Between The Two Steps")
+    plt.ylim(0,1.03)
+    ax2 = plt.twinx()
+    print(bar2.get_xticks())
 
-    pointer_map = {ext_params[i]: symbols[i] for i in range(0, len(ext_params))}
-    #x = [elem[0] for elem in running_times[(ds,technique,t,isomorphism)]]
-    #y = [elem[1] for elem in running_times[(ds,technique,t,isomorphism)]]
-    sns.scatterplot(x, y, color=color_map[iso_technique], marker="o",
-                 label="baseline" if iso_technique == "naive" else "two step approach")#) + ("leading type" +t) if t != "" else "weakly con. comp.")
-    # plt.plot(x,y,color=color_map[ds_], marker=pointer_map[(technique,t)])
-sns.despine()
-plt.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
-plt.xlabel("Number of Process Executions")
-plt.ylabel("Calculation Equivalence Classes in s")
-plt.title("Running Time for Computing Equivalence Classes")
-plt.tight_layout()
+    x_ticks = []
+    width = 0.1111
+    for t in range(-3,0):
+        x_ticks.append(0+t*width)
+    for t in range(-3,1):
+        x_ticks.append(1+t*width)
+    for t in range(-3,0):
+        x_ticks.append(2+t*width)
+    for t in range(-3,4):
+        x_ticks.append(3+t*width)
+    sns.lineplot(x_ticks,results_df['first_by_second'], color=colors[7], marker="o", ax=ax2)
+    ax2.set_ylabel('#Initial Classes divided by #Refined Classes', color=colors[7])
+    ax2.set(ylim=(0,1.03))
+    ax2.grid(False)
+    sns.despine()
+    plt.tight_layout()
+    #sns.catplot(x="dataset",y="first_step",hue="technique", kind="bar", data=results_df)
+    plt.savefig("first_second.png")
 
-plt.savefig("iso_sns_final.png")
-######### Isomorphism times
-#calculate number of variants for first and second step for all logs,
 
 #calculate comparison numbers for number of classes and running tim efrom naive baseline
 #for different subsets of cases, i.e., 10 percent, 20 percent etc.
