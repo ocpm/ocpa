fa40b19c6958a5ed7646fd335c653c183290e8c7
anonymous-author-1 <None>
2022-08-18 15:02:18 +0200

    Integrated logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,4 +54,5 @@ html_theme = 'bizstyle'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "_static/Logo_wide.png"
 napoleon_google_docstring = False
