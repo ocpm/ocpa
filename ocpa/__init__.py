1e1ec36c533b96c95b4b4037e1c4f5df863d3034
niklasadams <niklas.adams@pads.rwth-aachen.de>
2022-08-23 17:13:32 +0200

    documentation style

--- ocpa/__init__.py
+++ ocpa/__init__.py
@@ -3,9 +3,9 @@ import ocpa.objects
 import ocpa.util
 import ocpa.visualization
 
-__version__ = '0.0.30'
+__version__ = '1.0.0'
 __doc__ = "Object-Centric Process Analysis"
-__author__ = 'Gyunam Park and Niklas Adams'
-__author_email__ = 'gnpark@pads.rwth-aachen.de'
-__maintainer__ = 'Gyunam Park and Niklas Adams'
-__maintainer_email__ = "gnpark@pads.rwth-aachen.de"
+__author__ = 'Jan Niklas Adams and Gyunam Park'
+__author_email__ = 'niklas.adams@pads.rwth-aachen.de'
+__maintainer__ = 'Jan Niklas Adams and Gyunam Park'
+__maintainer_email__ = "niklas.adams@pads.rwth-aachen.de"
