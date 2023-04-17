f96b67e3c7a4e323491dbaa1a117449fec6295b5
gyunamister <gyunamister@gmail.com>
2023-04-17 18:57:11 +0200

    supporting xmlocel import

--- example-scripts/ocel2/e2o_qualifiers.py
+++ example-scripts/ocel2/e2o_qualifiers.py
@@ -3,9 +3,9 @@ from ocpa.objects.log.importer.ocel2.sqlite import factory as ocel_import_factor
 from ocpa.objects.log.importer.ocel2.xml import factory as xmlocel_import_factory
 
 # importing sqlite
-# ocel = ocel_import_factory.apply("../../sample_logs/ocel2/sqlite/running-example.sqlite")
+ocel = ocel_import_factory.apply("../../sample_logs/ocel2/sqlite/running-example.sqlite")
 # importing xmlocel
-ocel = xmlocel_import_factory.apply("../../sample_logs/ocel2/xmlocel/ocel2.xml")
+# ocel = xmlocel_import_factory.apply("../../sample_logs/ocel2/xmlocel/ocel2.xml")
 allowed, unallowed = e2o_qualifier_conformance(ocel, "send package", "employees", "forwarder", permitted_attributes = ["Warehousing"], attribute_lookup= "role")
 print("Allowed values")
 print(allowed)
