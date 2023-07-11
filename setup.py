29831d9c0cbd5e32681b20e5978ccbf7b629af3b
Gyunam Park <gyunamister@gmail.com>
2023-07-11 10:23:46 +0200

    fix tests

--- setup.py
+++ setup.py
@@ -20,18 +20,8 @@ setuptools.setup(
     # Provide either the link to your github or to your website
     url='https://github.com/ocpm/ocpa',
     install_requires=[            # I get to this in a second
-            "pm4py<2.3.0",
+            "pm4py==2.2.32",
             "setuptools",
             "jsonschema",
-    ],
-    # classifiers=[
-    # 'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    # 'Intended Audience :: Developers',      # Define that your audience are developers
-    # 'Topic :: Software Development :: Build Tools',
-    # 'License :: OSI Approved :: MIT License',   # Again, pick a license
-    # 'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    # 'Programming Language :: Python :: 3.4',
-    # 'Programming Language :: Python :: 3.5',
-    # 'Programming Language :: Python :: 3.6',
-    # ],
+    ]
 )
